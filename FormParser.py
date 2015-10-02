628eeb60de5ead1546be4b3a80433d8192821444
Sebastien Cagnon <cagnonsebastien@gmail.com>
2015-10-02 18:31:37 +0900

    not tested and need split of classes in files

--- FormParser.py
+++ FormParser.py
@@ -1,6 +1,66 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+class GFParser(object):
+	"""Allows to access all the questions from a GForm and submit a response"""
+
+	def __init__(self, url):
+		"""Loads form from its public page url"""
+		self.url = url
+		res = urllib2.urlopen(self.url)
+		self.soup = BeautifulSoup(res.read(), 'html.parser')
+		if len(self.soup.find_all("h1", "ss-form-title")) == 0:
+			raise ValueError("The url did not correspond to a valid Google Form")
+
+	def getFormInfos(self):
+		"""returns general form info [title, desc]"""
+		info = []
+		info.append(self.soup.find_all("h1", "ss-form-title")[0].string)
+		info.append(self.soup.find_all("div", "ss-form-desc")[0].string)
+		return info
+
+	def getQuestions(self):
+		"""Returns list of questions"""
+		questions = self.soup.form.findChildren("div", "ss-form-question")
+		# TODO format questions into usable classes
+		return questions
+
+
+class GFQuestion(object):
+	"""Generic API for Google Form Questions"""
+
+	def __init__(self, questionSoup):
+		"""Initializes from an ss-form-question div element as soup object"""
+		self.soup = questionSoup
+		self.type = soup.div["class"][1]
+		self.id   = soup.label["for"].replace("_", ".")
+		self.label = soup.label.div.text.strip()
+
+	def getType(self):
+		"""Returns question type (radio, text, ...) as string"""
+		return self.type
+
+	def getID(self):
+		"""Returns id of question in the form entry.### as string"""
+		return self.id
+
+	def getQuestionAsList(self):
+		"""Returns [id, type, question, choices]
+
+		choices depends on the question type.
+		For radio, select or checkbox, the choices are given as a list.
+		For text or paragraph-text, the choices is an empty string."""
+
+		retValue = [self.id, self.type, self.label]
+		if self.type in ["ss-radio", "ss-checkbox"]:
+			choices = [item.input["value"] for item in self.soup.findChildren("li")]
+			retValue.append(choices)
+		elif self.type in ["ss-text", "ss-paragraph-text"]:
+			retValue.append("")
+		elif self.type == "ss-select":
+			choices = [item.text for item in self.soup.findChildren["option"] if item.text!=""]
+			retValue.append(choices)
+		return retValue
 
 
 def main():
