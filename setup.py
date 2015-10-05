5ff6b96e8e5c7b1f79ec486c25d6fb754bbaf3b4
Sebastien Cagnon <cagnonsebastien@gmail.com>
2015-10-05 20:35:34 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'PyGoogleForm',
+  packages = ['PyGoogleForm'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple Python library to submit Google Forms in Python.',
+  author = 'Sebastien Cagnon',
+  author_email = 'cagnonsebastien@gmail.com',
+  url = 'https://github.com/sebcagnon/PyGoogleForm', # use the URL to the github repo
+  download_url = 'https://github.com/sebcagnon/PyGoogleForm/tarball/0.1',
+  keywords = ['Google Form'], # arbitrary keywords
+  classifiers = [],
+)
