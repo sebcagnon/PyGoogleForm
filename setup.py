f510e4dcf2fcba7e270e1f12127ce692e040440b
Sebastien Cagnon <cagnonsebastien@gmail.com>
2015-10-06 12:51:17 +0900

    adding bs4 dependency in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'PyGoogleForm',
   packages = ['PyGoogleForm'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'A simple Python library to submit Google Forms in Python.',
   author = 'Sebastien Cagnon',
   author_email = 'cagnonsebastien@gmail.com',
-  url = 'https://github.com/sebcagnon/PyGoogleForm', # use the URL to the github repo
+  url = 'https://github.com/sebcagnon/PyGoogleForm',
   download_url = 'https://github.com/sebcagnon/PyGoogleForm/tarball/0.2',
-  keywords = ['Google Form'], # arbitrary keywords
+  keywords = ['Google Form'],
+  install_requires = ['beautifulsoup4']
   classifiers = [],
 )
