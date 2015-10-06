ba42b8f5dcda07d39d375c33525f55153412f3bc
Sebastien Cagnon <cagnonsebastien@gmail.com>
2015-10-06 12:30:56 +0900

    bumped version in setup file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'PyGoogleForm',
   packages = ['PyGoogleForm'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A simple Python library to submit Google Forms in Python.',
   author = 'Sebastien Cagnon',
   author_email = 'cagnonsebastien@gmail.com',
   url = 'https://github.com/sebcagnon/PyGoogleForm', # use the URL to the github repo
-  download_url = 'https://github.com/sebcagnon/PyGoogleForm/tarball/0.1',
+  download_url = 'https://github.com/sebcagnon/PyGoogleForm/tarball/0.2',
   keywords = ['Google Form'], # arbitrary keywords
   classifiers = [],
 )
