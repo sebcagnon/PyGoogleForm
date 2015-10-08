62054749b65613aa6e74d67e0512f5a7699701be
Sebastien Cagnon <cagnonsebastien@gmail.com>
2015-10-08 10:28:31 +0900

    corrected bug in setup.py script

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from distutils.core import setup
 
 # setting up long description as rst if pandoc is available
 # if pandoc is unavailable, uses the .md anyway
-long_description = open("README.md").read()
+long_description = ""
 try:
     import subprocess
     import pandoc
+
+    open("README.md").read()
 
     process = subprocess.Popen(
         ['which pandoc'],
@@ -21,21 +23,21 @@ try:
     doc.markdown = long_description
 
     long_description = doc.rst
-except:
+except ImportError, IOError:
     pass
 
 
 setup(
   name = 'PyGoogleForm',
   packages = ['PyGoogleForm'], # this must be the same as the name above
-  version = '0.5',
+  version = '0.6',
   description = 'A simple Python library to submit Google Forms in Python.',
   long_description = long_description,
   author = 'Sebastien Cagnon',
   author_email = 'cagnonsebastien@gmail.com',
   url = 'https://github.com/sebcagnon/PyGoogleForm',
-  download_url = 'https://github.com/sebcagnon/PyGoogleForm/tarball/0.5',
-  licence = 'MIT',
+  download_url = 'https://github.com/sebcagnon/PyGoogleForm/tarball/0.6',
+  license = 'MIT',
   keywords = ['Google Form'],
   install_requires = ['beautifulsoup4'],
   classifiers = [],
